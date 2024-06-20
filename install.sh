0b9dea95976fc697528ea4b460096a12236b4c80
Xorre AI <miker@xorre.com>
2024-06-20 12:32:53 -0400

    path fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo bash prereq-install.sh
-sudo cp -r ../Arpir-Obfuscation-Engine/ /usr/local/bin/
+sudo cp -r /tmp/Arpir-Obfuscation-Engine/ /usr/local/bin/
 sudo chmod +x /usr/local/bin/Arpir-Obfuscation-Engine/*.sh
 
