0dc203dfc50720dce60552506267628c971ff89c
Xorre AI <miker@xorre.com>
2024-11-22 11:30:44 -0500

    update mint

--- _bin/mint-install.sh
+++ _bin/mint-install.sh
@@ -8,15 +8,13 @@ sudo apt update -y
 sudo apt install -y sublime-text
 
 # Install encfs, wget, and expect
-sudo apt install -y encfs wget expect
+sudo apt install -y encfs wget expect git
 
 # Install development tools
 sudo apt install -y gcc libc6-dev libsodium-dev make autoconf unrar
 
-# Install Plasma Discover, Snapd, and Steghide
-sudo apt install -y plasma-discover snapd
-sudo ln -s /var/lib/snapd/snap /snap
-sudo apt install -y steghide
+# Install Plasma Discover and Steghide
+sudo apt install -y plasma-discover steghide
 
 # Install Tor Browser Launcher
 sudo apt install -y torbrowser-launcher
@@ -24,13 +22,17 @@ sudo apt install -y torbrowser-launcher
 # Install GIMP, VLC, Chromium, LibreOffice, and FileZilla
 sudo apt install -y gimp vlc chromium-browser libreoffice filezilla
 
-# Install Opera using Snap
-sudo snap install opera
+# Install Flatpak support and add Flathub repository
+sudo apt install -y flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Optional Snap installs (uncomment as needed)
-sudo snap install signal-desktop
-sudo snap install telegram-desktop
-sudo snap install slack
-sudo snap install discord
-sudo snap install whatsie
-sudo snap install gallery-dl
+# Install Opera using Flatpak
+flatpak install -y flathub com.opera.Opera
+
+# Install other apps using Flatpak (replacing Snap)
+flatpak install -y flathub org.signal.Signal           # Signal Desktop
+flatpak install -y flathub org.telegram.desktop        # Telegram Desktop
+flatpak install -y flathub com.slack.Slack             # Slack
+flatpak install -y flathub com.discordapp.Discord      # Discord
+flatpak install -y flathub io.github.mimbrero.Whatsie  # Whatsie
+flatpak install -y flathub io.github.gilbertchen.gallery_dl # Gallery-dl
